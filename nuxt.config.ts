4f98bab1cfdb9c155e9832095ac35eba1b6526ce
Daniel Chen <dchen024@citymail.cuny.edu>
2023-08-30 00:08:03 -0400

    change baseURL to REAL vercel URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,34 @@
 import { resolve } from 'node:path';
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: 'https://hipe-network.vercel.app/',
+  },
   modules: ['@hebilicious/authjs-nuxt', '@element-plus/nuxt', '@unocss/nuxt'],
   css: ['@/assets/scss/main.scss'],
   runtimeConfig: {
     authJs: {
-      secret: process.env.NUXT_NEXTAUTH_SECRET, // You can generate one with `openssl rand -base64 32`
+      secret: import.meta.env.NUXT_NEXTAUTH_SECRET, // You can generate one with `openssl rand -base64 32`
     },
     google: {
-      clientId: process.env.NUXT_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NUXT_GOOGLE_CLIENT_SECRET,
-      redirectProxyUrl: process.env.NUXT_GOOGLE_REDIRECT_PROXY_URL,
+      clientId: import.meta.env.NUXT_GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.NUXT_GOOGLE_CLIENT_SECRET,
+      redirectProxyUrl: import.meta.env.NUXT_GOOGLE_REDIRECT_PROXY_URL,
     },
     linkedin: {
-      clientId: process.env.NUXT_LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.NUXT_LINKEDIN_CLIENT_SECRET,
-      redirectProxyUrl: process.env.NUXT_LINKEDIN_REDIRECT_PROXY_URL,
+      clientId: import.meta.env.NUXT_LINKEDIN_CLIENT_ID,
+      clientSecret: import.meta.env.NUXT_LINKEDIN_CLIENT_SECRET,
+      redirectProxyUrl: import.meta.env.NUXT_LINKEDIN_REDIRECT_PROXY_URL,
+    },
+    huggingface: {
+      apiKey: import.meta.env.NUXT_HUGGINGFACE_API_KEY,
+    },
+    mongodb: {
+      uri: import.meta.env.NUXT_MONGODB_URI,
     },
     public: {
       authJs: {
-        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        baseUrl: import.meta.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
         verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
       },
     },
@@ -28,7 +37,7 @@ export default defineNuxtConfig({
     verifyClientOnEveryRequest: true,
     guestRedirectTo: '/',
     authenticatedRedirectTo: '/home',
-    baseUrl: 'http://localhost:3000', // should be something like https://www.my-app.com
+    baseUrl: 'https://hipe-network.vercel.app/', // should be something like https://www.my-app.com
   },
   elementPlus: {
     importStyle: 'scss',
